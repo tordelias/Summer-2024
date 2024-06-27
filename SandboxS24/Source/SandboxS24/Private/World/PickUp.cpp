30ecc36cbce4d077d74b65e704a09b29c0e69f19
Tord-Elias <tordeliashoumb46@gmail.com>
2024-06-27 13:34:03 +0200

    Added PickUp class
    The PickUp class gets information on the item and sends it to the (ToBeImplemented) Inventory

---
+++ SandboxS24/Source/SandboxS24/Private/World/PickUp.cpp
@@ -0,0 +1,122 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "World/PickUp.h"
+#include "Itembase.h"
+#include "ItemDataStructs.h"
+
+// Sets default values
+APickUp::APickUp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	PickUpMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickupMesh"); 
+	PickUpMesh->SetSimulatePhysics(true); 
+	SetRootComponent(PickUpMesh); 
+
+}
+void APickUp::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitalizePickup(UItembase::StaticClass(), ItemQuantity);
+
+}
+
+void APickUp::InitalizePickup(const TSubclassOf<UItembase> BaseClass, const int32 InQuantity)
+{
+	if (ItemDataTable && !DesieredItemID.IsNone())
+	{
+		const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesieredItemID, DesieredItemID.ToString());
+
+		ItemRefrence = NewObject<UItembase>(this, BaseClass);
+
+		ItemRefrence->ID = ItemData->ID; 
+		ItemRefrence->ItemType = ItemData->ItemType; 
+		ItemRefrence->ItemQuality = ItemData->ItemQuality; 
+		ItemRefrence->ItemNumericData = ItemData->ItemNumericData; 
+		ItemRefrence->ItemTextData = ItemData->ItemTextData; 
+		ItemRefrence->ItemAssetData = ItemData->ItemAssetData; 
+
+		InQuantity <= 0 ? ItemRefrence->SetQuantity(1) : ItemRefrence->SetQuantity(InQuantity); 
+
+		PickUpMesh->SetStaticMesh(ItemData->ItemAssetData.Mesh);
+
+		UpdateInteractableData(); 
+	}
+}
+
+void APickUp::InitalizeDrop(UItembase* ItemDrop, const int32 InQuantity)
+{
+	ItemRefrence = ItemDrop; 
+	InQuantity <= 0 ? ItemRefrence->SetQuantity(1) : ItemRefrence->SetQuantity(InQuantity);
+	ItemRefrence->ItemNumericData.Weight = ItemDrop->GetItemSingleWeight(); 
+	PickUpMesh->SetStaticMesh(ItemDrop->ItemAssetData.Mesh);
+	UpdateInteractableData(); 
+}
+
+void APickUp::BeginFocus()
+{
+	if (PickUpMesh)
+	{
+		PickUpMesh->SetRenderCustomDepth(true);
+	}
+}
+
+void APickUp::EndFocus()
+{
+	if (PickUpMesh)
+	{
+		PickUpMesh->SetRenderCustomDepth(false);
+	}
+}
+
+void APickUp::Interact(AMCharacter* PlayerCharacter)
+{
+	if (PlayerCharacter)
+	{
+		TakePickup(PlayerCharacter);
+	}
+}
+
+// Called when the game starts or when spawned
+
+void APickUp::TakePickup(const AMCharacter* Taker)
+{
+	if (!IsPendingKillPending())
+	{
+		if (ItemRefrence)
+		{
+			//if(UInventoryComponent* PlayerInventory = Taker->GetInventory())
+		}
+	}
+}
+
+void APickUp::UpdateInteractableData()
+{
+	InstanceInteractableData.InteractableType = EInteractableType::Pickup;
+	InstanceInteractableData.Action = ItemRefrence->ItemTextData.IteractionText;
+	InstanceInteractableData.Name = ItemRefrence->ItemTextData.Name; 
+	InstanceInteractableData.Quantity = ItemRefrence->Quantity; 
+	InteractableData = InstanceInteractableData; 
+}
+
+void APickUp::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName ChangedPropertyName = PropertyChangedEvent.Property ? PropertyChangedEvent.Property->GetFName() : NAME_None; 
+
+	if (ChangedPropertyName == GET_MEMBER_NAME_CHECKED(APickUp, DesieredItemID))
+	{
+		if (ItemDataTable)
+		{
+			if (const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesieredItemID, DesieredItemID.ToString()))
+			{
+				PickUpMesh->SetStaticMesh(ItemData->ItemAssetData.Mesh);
+			}
+		}
+	}
+}
+
---
+++ SandboxS24/Source/SandboxS24/Public/World/PickUp.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/InteractionInterface.h"
+#include "PickUp.generated.h"
+
+class UItembase;
+class UDataTable; 
+
+UCLASS()
+class SANDBOXS24_API APickUp : public AActor, public IInteractionInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickUp();
+
+
+
+	void InitalizePickup(const TSubclassOf<UItembase> BaseClass, const int32 InQuantity);
+
+	void InitalizeDrop(UItembase* ItemDrop, const int32 InQuantity);
+
+	FORCEINLINE UItembase* GetItemData() { return ItemRefrence; }
+
+	virtual void BeginFocus() override; 
+
+	virtual void EndFocus() override; 
+
+	virtual void Interact(AMCharacter* PlayerCharacter) override; 
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Pickup")
+	UStaticMeshComponent* PickUpMesh; 
+
+	UPROPERTY(EditInstanceOnly, Category = "Pickup")
+	UDataTable* ItemDataTable;
+
+	UPROPERTY(EditInstanceOnly, Category = "Pickup")
+	FName DesieredItemID;
+
+	UPROPERTY(VisibleAnywhere, Category = "Pickup")
+	UItembase* ItemRefrence;
+
+	UPROPERTY(EditInstanceOnly, Category = "Pickup")
+	int32 ItemQuantity;
+
+	UPROPERTY(VisibleInstanceOnly, Category = "Pickup")
+	FInteractableData InstanceInteractableData;
+
+
+	void TakePickup(const AMCharacter* Taker);
+
+	void UpdateInteractableData(); 
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override; 
+#endif
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
